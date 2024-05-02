a65fa0741de0f8f60ee8e75ba4168a71e3106c34
SaurabhBhatiaodesk <s.bhatiaodesk@gmail.com>
2024-05-02 15:16:52 +0530

    new coded added

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -5,6 +5,13 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
+
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export default function App() {
   return (
@@ -13,8 +20,7 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.0.5/tailwind.min.css" />
-        <link rel="stylesheet" href="https://cdn.shopify.com/s/files/1/0843/1642/2421/files/stylesheet.css?v=1713961441" />
+         <link rel="stylesheet" href="https://cdn.shopify.com/s/files/1/0843/1642/2421/files/stylesheet.css?v=1713961441" />
         <link
           rel="stylesheet"
           href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
--- app/app/routes/app._index.tsx
+++ app/app/routes/app._index.tsx
@@ -1,124 +1,137 @@
-import { useEffect } from "react";
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import '../components/Onboarding/onboarding.css'
-import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
-import {
-  Page,
-  Layout,
-  Text,
-  Card,
-  Button,
-  BlockStack,
-  Box,
-  List,
-  Link,
-  InlineStack,
-} from "@shopify/polaris";
+// import { json } from "@remix-run/node";
+import FirstHeader from "~/components/FirstHeader";
+import SecondHeader from "~/components/SecondHeader";
+import { useActionData,  useSubmit } from "@remix-run/react";
+
+import React, { useState, useEffect } from "react";
+
 import { authenticate } from "../shopify.server";
-import handleRequest from "~/entry.server";
-import Onboarding from "~/components/Onboarding/Onboarding";
+import CustomSlider from "~/components/CustomSilder";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
+  const { admin } = await authenticate.admin(request);
 
-  return null;
+  const response = await admin.graphql(
+    `#graphql
+    query getProducts {
+      products (first: 250) {
+        edges {
+          node {
+            id
+            title
+          }
+        }
+      }
+    }`,
+  );
+
+  const data = await response.json();
+  return { data };
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { admin } = await authenticate.admin(request);
-  const color = ["Red", "Orange", "Yellow", "Green"][
-    Math.floor(Math.random() * 4)
-  ];
-  const response = await admin.graphql(
-    `#graphql
-      mutation populateProduct($input: ProductInput!) {
-        productCreate(input: $input) {
-          product {
-            id
-            title
-            handle
-            status
-            variants(first: 10) {
-              edges {
-                node {
-                  id
-                  price
-                  barcode
-                  createdAt
-                }
-              }
-            }
-          }
-        }
-      }`,
-    {
-      variables: {
-        input: {
-          title: `${color} Snowboard`,
-        },
-      },
-    },
-  );
-  const responseJson = await response.json();
+  const body = await request.formData();
+  const inputNumber = body.get("productShowBaseOnInuptNumber");
+  const searchProdcutsQuery = body.get("searchProductBaseOnTitle");
+  console.log(searchProdcutsQuery)
 
-  const variantId =
-    responseJson.data!.productCreate!.product!.variants.edges[0]!.node!.id!;
-  const variantResponse = await admin.graphql(
-    `#graphql
-      mutation updateVariant($input: ProductVariantInput!) {
-        productVariantUpdate(input: $input) {
-          productVariant {
-            id
-            price
-            barcode
-            createdAt
-          }
-        }
-      }`,
-    {
-      variables: {
-        input: {
-          id: variantId,
-          price: Math.random() * 100,
-        },
-      },
-    },
-  );
-
-  const variantResponseJson = await variantResponse.json();
-
-  return json({
-    product: responseJson!.data!.productCreate!.product,
-    variant: variantResponseJson!.data!.productVariantUpdate!.productVariant,
-  });
-};
-
-export default function Index() {
-  const nav = useNavigation();
-  const actionData = useActionData<typeof action>();
-  const submit = useSubmit();
-  const isLoading =
-    ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
-  const productId = actionData?.product?.id.replace(
-    "gid://shopify/Product/",
-    "",
-  );
-
-  useEffect(() => {
-    if (productId) {
-      shopify.toast.show("Product created");
-    }
-  }, [productId]);
-  const generateProduct = () => submit({}, { replace: true, method: "POST" });
-  
-  const handleButton = ()=>{
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>aaaaa")
+  let convertInNum: number | null = null;
+  if (inputNumber !== null) {
+    convertInNum = parseInt(inputNumber as string, 10);
   }
 
+  let graphqlQuery = `#graphql
+    query getProducts {
+      products`;
+  if (convertInNum !== null && !isNaN(convertInNum)) {
+    graphqlQuery += `(first:${convertInNum},query:"${searchProdcutsQuery}")`;
+  }
+  graphqlQuery += ` {
+        edges {
+          node {
+            id
+            title 
+          }
+        }
+      }
+    }`;
+
+  const response = await admin.graphql(graphqlQuery);
+  const data = await response.json();
+  console.log(data);
+  return { data }; // returning the GraphQL response data
+};
+
+
+export default function Index() {
+  let submit = useSubmit();
+  const productData =  useActionData<typeof action>();
+  const [firstTabColor, setFirstTabColor] = useState("blue");
+  const [tabStep, setTabstep] = useState("1");
+  const [secondTabColor, setSecondTabColor] = useState("");
+  const [inputData, setInputData] = useState<number>(5);
+  const [searchInputData, setSearchInputData] = useState<string>("")
+  const [activeHeader, setActiveHeader] = useState("first");
+  const productTitles: string[] =
+    productData?.data?.data?.products?.edges || [];
+
+
+    console.log(productData ?? "productDataproductData","productData")
+
+    useEffect(() => {
+      if (searchInputData) {
+        console.log("Search data received in action function: ", searchInputData); // Log search data received from child component
+      }
+    }, [searchInputData]);
+
+  useEffect(() => {
+    if (inputData) {
+      // Automatically submit the form if inputData is available
+      submit({ productShowBaseOnInuptNumber: inputData, searchProductBaseOnTitle:searchInputData }, { method: "post" });
+      setInputData(inputData);
+    }
+  }, [inputData,searchInputData, submit]);
+
+  const handleHeaderChange = (header: string) => {
+    if (header === "first") {
+      setActiveHeader(header);
+      setSecondTabColor("");
+      setFirstTabColor("blue");
+      setTabstep("1")
+    } else if (header === "second") {
+      setActiveHeader(header);
+      setSecondTabColor("blue");
+      setFirstTabColor("green");
+      setTabstep("2");
+    }
+  };
   return (
     <>
-    <Onboarding/>
+  <div className="container w-full" style={{ maxWidth: "100%",background:"#fff",padding:"20px 50px" }}> 
+     <h1 className="mb-2 onbording_step_title">Onboarding - {tabStep} of 2 steps</h1>
+      <div className="onboarding_steps  w-ful">
+        <button
+          onClick={() => handleHeaderChange("first")}
+          style={{ backgroundColor: firstTabColor }}
+        ></button>
+
+        <button
+          onClick={() => handleHeaderChange("second")}
+          style={{ backgroundColor: secondTabColor }}
+        ></button>
+      </div>
+
+      {activeHeader === "first" && (
+        <FirstHeader
+          productTitles={productTitles}
+          sendDataToParent={setInputData}
+          sendDataToParentToSearchValues={setSearchInputData}
+        />
+      )}
+      {activeHeader === "second" && <SecondHeader />}
+      </div>
     </>
   );
 }
