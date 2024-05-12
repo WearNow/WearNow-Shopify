87c77e506b373eb9f5b74bb9dfcd20f72738ba8f
xhx2024 <xhx@wearnow.ai>
2024-05-12 22:48:13 +0800

    Update vps page

---
+++ app/components/MySteps.tsx
@@ -0,0 +1,83 @@
+import { Card } from '@shopify/polaris';
+
+import React from 'react';
+
+interface StepPropItem {
+  status?: 'waiting' | 'processing' | 'completed';
+  title: string;
+  sutitle?: React.ReactNode | string | undefined;
+}
+interface StepperProps {
+  currentStep: number;
+  items: StepPropItem[];
+  onNext?: (step: number) => void;
+}
+
+const Stepper: React.FC<StepperProps> = (prop: StepperProps) => {
+  const { currentStep, items } = prop;
+
+  // waiting, processing, completed
+  const isWaitingStep = (index: number) => index > currentStep;
+  const isProcessingStep = (index: number) => index === currentStep;
+  const isCompletedStep = (index: number) => index < currentStep;
+
+  const circleColor = (index: number) => (isWaitingStep(index) ? 'border-gray' : isProcessingStep(index) ? 'border-dashed border-black' : isCompletedStep(index) ? 'border-green-600' : 'bg-gray-300');
+  const circleTextColor = (index: number) => (isWaitingStep(index) ? 'text-gray-300' : isProcessingStep(index) ? 'text-black' : isCompletedStep(index) ? 'text-green-600' : 'text-gray-500');
+  const titleColor = (index: number) => (isWaitingStep(index) ? 'text-gray-300' : 'text-black');
+  const lineColor = (index: number) => (isWaitingStep(index) ? 'border-black' : 'bg-black');
+  return (
+    <div>
+      {items.map((_, index) => (
+        <div className="w-full flex items-start justify-start mb-2" key={index}>
+          <React.Fragment key={index}>
+            <div>
+              {/* circle */}
+              <div className="w-full flex items-center justify-start ">
+                <div className={`w-9 h-9 bg-white rounded-full border justify-center items-center inline-flex ${circleColor(index)}`}>
+                  <div className={`text-center text-sm font-bold font-['SF Pro'] leading-tight ${circleTextColor(index)}`}>0{index + 1}</div>
+                </div>
+              </div>
+              {/* line */}
+              {isWaitingStep(index) ? null : <div style={{ width: '2px', marginLeft: '17px' }} className={`h-20 ${lineColor(index)}`}></div>}
+            </div>
+            {/* input or img */}
+
+            <div className="ml-2">
+              {/* title */}
+              <div className={`w-full h-9 text-black/opacity-20 text-xl font-medium font-['SF Pro Display'] leading-normal ${titleColor(index)}`}>{_.title}</div>
+              {/* sutitle */}
+              {isProcessingStep(index) ? <div className="w-full">{_.sutitle}</div> : null}
+            </div>
+          </React.Fragment>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const MySetps: React.FC<StepperProps> = (prop: StepperProps) => {
+  const { currentStep, items, onNext: onStepChange } = prop;
+
+  const NextButton = (
+    <div
+      className="w-20 opacity-40 justify-center items-end inline-flex"
+      onClick={() => {
+        if (onStepChange) onStepChange(currentStep);
+      }}
+    >
+      <div className="px-4 py-2.5 bg-sky-600 rounded-full justify-center items-center gap-2 flex">
+        <div className="text-white text-base font-medium font-['SF Pro Display'] leading-normal">Next</div>
+        <div className="w-1 h-2 relative" />
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="w-full h-full">
+      <Stepper currentStep={currentStep} items={items} />
+      {NextButton}
+    </div>
+  );
+};
+
+export default MySetps;
--- app/routes/app.photostudio.tsx
+++ app/routes/app.photostudio.tsx
@@ -1,37 +1,93 @@
-import {
-    Box,
-    Card,
-    Layout,
-    Link,
-    List,
-    Page,
-    Text,
-    BlockStack,
-  } from "@shopify/polaris";
-import Dashboard from "~/components/Dashboard";
-  
-  export default function PhotostudioPage() {
-    return (
-      <>
-      <Dashboard/>
-      </>
-    );
-  }
-  
-  function Code({ children }: { children: React.ReactNode }) {
-    return (
-      <Box
-        as="span"
-        padding="025"
-        paddingInlineStart="100"
-        paddingInlineEnd="100"
-        background="bg-surface-active"
-        borderWidth="025"
-        borderColor="border"
-        borderRadius="100"
-      >
-        <code>{children}</code>
-      </Box>
-    );
-  }
-  
+import React, { useState } from "react";
+import DashboardHeader from "~/components/DashboardHeader";
+import MySetps from "~/components/MySteps";
+import ProductSelector from "~/components/ProductoSelector";
+import ThumbnailSelector from "~/components/ThumbnailSelector";
+
+const LeftTop: React.FC = () => {
+  return (
+    <>
+      <div className="w-full h-50 mb-6 flex-col justify-start items-start gap-4 inline-flex">
+        <div className="text-neutral-800 text-4xl font-medium font-['SF Pro Display'] leading-normal">
+          Create Product Photos
+        </div>
+        <div className="w-full text-zinc-600 text-base font-medium font-['SF Pro Display'] leading-normal">
+          Take your product photos to the next level in just 4 easy steps!
+        </div>
+      </div>
+    </>
+  );
+};
+
+function App() {
+  const description = "hhhh";
+
+  const [items, setItmes] = React.useState([
+    {
+      title: "Select 1 Product",
+      sutitle: (
+        <>
+          {/* subtitle */}
+          <div className="w-full h-full pl-0.5 py-1 gap-1 ">
+            <div className="text-zinc-600 text-sm font-medium font-['SF Pro Display'] leading-none">
+              Select the number of photos you would like to create
+            </div>
+          </div>
+          {/* input */}
+          <div className="w-full h-20 flex-col justify-start items-start gap-1 inline-flex">
+            <div className="self-stretch p-3 bg-white rounded-md border border-neutral-400 justify-start items-center inline-flex">
+              <div className="w-2.5 justify-start items-center flex">
+                <div className="text-neutral-400 text-base font-normal font-['SF Pro Display']">
+                  4
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ),
+    },
+    {
+      title: "Select a model",
+      sutitle: description,
+    },
+    {
+      title: "Select a background",
+      sutitle: description,
+    },
+    {
+      title: "Select a pose",
+      sutitle: description,
+    },
+  ]);
+
+  const [currentStep, setCurrentStep] = useState<number>(0);
+
+  const renderRight = () => {
+    switch (currentStep) {
+      case 0:
+        return <ProductSelector />;
+      case 1:
+        return <ThumbnailSelector />;
+      default:
+        return <ThumbnailSelector />;
+    }
+  };
+  return (
+    <>
+      <DashboardHeader />
+      <div className="w-full flex justify-between items-start bg-white px-[60px] my-[40px]">
+        <div className="h-full w-400">
+          <LeftTop />
+          <MySetps
+            items={items}
+            currentStep={currentStep}
+            onNext={(step) => setCurrentStep(step + 1)}
+          />
+        </div>
+        <div className="h-[720px] w-280">{renderRight()}</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
