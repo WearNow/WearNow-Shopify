6ed13c263b612f0bec73a1f255313d6d09600392
xhx2024 <xhx@wearnow.ai>
2024-05-20 21:01:05 +0800

    add header to history page

--- app/routes/app.history.tsx
+++ app/routes/app.history.tsx
@@ -1,9 +1,11 @@
+import DashboardHeader from "~/components/DashboardHeader";
 import HistoryPhotos from "~/components/HistoryPhotos";
 import HistoryQueue from "~/components/HistoryQueue";
 
 export default function () {
   return (
     <div className="bg-white">
+      <DashboardHeader></DashboardHeader>
       <HistoryQueue></HistoryQueue>
       <HistoryPhotos></HistoryPhotos>
     </div>
