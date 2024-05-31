38c6ab0746859e5aeafd16e5494f3a72d3c1d108
Saurab Bhatia <S.Bhatiaodesk@gmail.com>
2024-05-31 15:43:36 +0530

    product select/unselect

--- app/components/FirstHeader.tsx
+++ app/components/FirstHeader.tsx
@@ -28,6 +28,9 @@ const FirstHeader: React.FC<{ sessionData: any, onActivate: any }> = ({ sessionD
   const toggleModal = () => {
     setModals(!modals);
   };
+  useEffect(()=>{
+    handleInputChange;
+  })
   const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = parseInt(event.target.value);
     if (!isNaN(newValue)) {
