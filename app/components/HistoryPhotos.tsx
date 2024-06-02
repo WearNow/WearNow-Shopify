594fc26c017a26aadac49db8436d8c0a055a8501
xhx2024 <xhx@wearnow.ai>
2024-06-02 22:14:14 +0800

    connect with product_photo_history api

--- app/components/HistoryPhotos.tsx
+++ app/components/HistoryPhotos.tsx
@@ -1,6 +1,8 @@
-import * as React from 'react';
-import { FC } from 'react';
-import { HistoryModal } from '~/components/HistoryModal';
+import gql from "graphql-tag";
+import * as React from "react";
+import { FC } from "react";
+import { HistoryModal } from "~/components/HistoryModal";
+import client from "~/services/ApolloClient";
 
 type ImageWithAltProps = {
   src: string;
@@ -8,7 +10,15 @@ type ImageWithAltProps = {
   className?: string;
 };
 
-const ImageWithAlt: FC<ImageWithAltProps> = ({ src, alt, className }) => <img loading="lazy" src={src} alt={alt} className={className} style={{ height: '100%' }} />;
+const ImageWithAlt: FC<ImageWithAltProps> = ({ src, alt, className }) => (
+  <img
+    loading="lazy"
+    src={src}
+    alt={alt}
+    className={className}
+    style={{ height: "100%" }}
+  />
+);
 
 type CardProps = {
   src: string;
@@ -20,164 +30,243 @@ type CardProps = {
   onClick?: () => void;
 };
 
-const Card: FC<CardProps> = ({ src, name, photos, date, time, aspect, onClick }) => (
+const Card: FC<CardProps> = ({
+  src,
+  name,
+  photos,
+  date,
+  time,
+  aspect,
+  onClick,
+}) => (
   <div
     // style={{ width: '238px', height: '238px' }}
     onClick={onClick}
-    className={`cursor-pointer flex-shrink-0 flex overflow-hidden relative flex-col justify-center rounded-lg p-2.5 ${aspect === 'square' ? 'aspect-square' : 'aspect-[1.34]'}`}
+    className={`cursor-pointer flex-shrink-0 flex overflow-hidden relative flex-col justify-center rounded-lg p-2.5 ${aspect === "square" ? "aspect-square" : "aspect-[1.34]"}`}
   >
-    <ImageWithAlt src={src} alt={name} className=" object-cover absolute inset-0 size-full" />
+    <ImageWithAlt
+      src={src}
+      alt={name}
+      className=" object-cover absolute inset-0 size-full"
+    />
     <div className="flex flex-col h-full items-start py-5 px-5 rounded-lg border border-white border-solid shadow-lg backdrop-blur-[6px] max-md:pr-5">
-      <div className="text-xl sm:text-lg font-semibold">{name}</div>
-      <div className="text-base mt-[75%] sm:text-sm sm:mt-[50%] md:text-lg md:mt-[50%]">{photos} Photos</div>
-      <div className="text-base sm:text-sm md:text-lg mt-2.5">
+      <div className="text-xl sm:text-md md:text-xl lg:text-base xl:text-2xl font-semibold">
+        {name}
+      </div>
+      <div className="text-base sm:text-base md:text-lg lg:text-sm xl:text-xl mt-auto">
+        {photos} Photos
+      </div>
+      <div className="text-base sm:text-base md:text-lg lg:text-sm xl:text-xl">
         {date} | {time}
       </div>
     </div>
   </div>
 );
-// const Card: FC<CardProps> = ({ src, name, photos, date, time, aspect }) => (
-//   <div className={`flex overflow-hidden relative flex-col justify-center p-2.5 ${aspect === "square" ? "aspect-square" : "aspect-[1.34]"}`}>
-//     <ImageWithAlt src={src} alt={name} className="object-cover absolute inset-0 size-full" />
-//     <div className="flex relative flex-col items-start py-5 pr-16 pl-5 rounded-lg border border-white border-solid shadow-lg backdrop-blur-[6px] max-md:pr-5">
-//       <div className="text-base font-semibold">{name}</div>
-//       <div className={`mt-${aspect === "square" ? "32" : "32"} text-sm max-md:mt-10`}>{photos} Photos</div>
-//       <div className="mt-3.5 text-xs">{date} | {time}</div>
-//     </div>
-//   </div>
-// );
 
 const HistoryPhotos: FC = () => {
-  // const images = [
-  //   { src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328", alt: "Image 3" },
-  //   { src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-2.jpg?v=1716110328", alt: "Image 4" },
-  //   { src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-3.jpg?v=1716110329", alt: "Image 5" },
-  //   { src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-4.jpg?v=1716110328", alt: "Image 6" },
-  // ];
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [historyData, setHistoryData] = React.useState([]);
 
-  const cards = [
+  const fetchHistoryData = async () => {
+    await client
+      .query({
+        query: gql`
+          query MyQuery {
+            product_photo_history {
+              generated_image
+              created_at
+              uuid
+              store_product {
+                images
+                product_id
+                title
+              }
+            }
+          }
+        `,
+        fetchPolicy: "network-only",
+      })
+      .then((result) => {
+        console.log("apollo client result: :::", result);
+        var product_photo_history = result.data.product_photo_history;
+        // Map over store_products to create a new array with the added 'image' property
+        function formatTimeWithAMPM(dateTimeString:string) {  
+          // 解析日期时间字符串  
+          const dateTime = new Date(dateTimeString);  
+        
+          // 提取小时和分钟  
+          const hours = dateTime.getHours();  
+          const minutes = dateTime.getMinutes().toString().padStart(2, '0');  
+        
+          // 判断AM/PM并添加标记  
+          const ampm = hours >= 12 ? 'PM' : 'AM';  
+          const formattedHours = hours % 12 || 12; // 将24小时制转换为12小时制，并处理午夜的情况  
+        
+          // 格式化时间并返回  
+          return `${formattedHours}:${minutes} ${ampm}`;  
+      }  
+        const updatedStoreProducts = product_photo_history.map(
+          (pph: any, index: number) => {
+            //   // Assuming sp.images is already a JSON string that needs to be parsed
+            return {
+              src: JSON.parse(pph.generated_image).images[0],
+              name: pph.store_product.title,
+              photos: JSON.parse(pph.generated_image).images.length,
+              date: pph.created_at.split("T")[0],
+              time: formatTimeWithAMPM(pph.created_at)
+            };
+          }
+        );
+
+        setHistoryData(updatedStoreProducts);
+        console.log(
+          "apollo client updatedStoreProducts: :::",
+          updatedStoreProducts
+        );
+      });
+  };
+
+  React.useEffect(() => {
+    // Call the fetchProducts function when the component mounts
+    console.log("apollo useEffect :::");
+    fetchHistoryData();
+  }, []);
+
+  const demoCards = [
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name]",
       photos: 4,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'square'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "square",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-2.jpg?v=1716110328',
-      name: '[Product Name 2]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-2.jpg?v=1716110328",
+      name: "[Product Name 2]",
       photos: 10,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'square'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "square",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name 3]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name 3]",
       photos: 10,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'square'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "square",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name 4]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name 4]",
       photos: 2,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'square'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "square",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name 5]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name 5]",
       photos: 1,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'square'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "square",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name 6]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name 6]",
       photos: 4,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'rectangle'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "rectangle",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name 7]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name 7]",
       photos: 10,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'rectangle'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "rectangle",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name 8]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name 8]",
       photos: 10,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'rectangle'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "rectangle",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name 9]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name 9]",
       photos: 2,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'rectangle'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "rectangle",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name 10]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name 10]",
       photos: 1,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'rectangle'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "rectangle",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name 10]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name 10]",
       photos: 1,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'rectangle'
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "rectangle",
     },
     {
-      src: 'https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328',
-      name: '[Product Name 10]',
+      src: "https://cdn.shopify.com/s/files/1/0641/2268/3542/files/gallery-1.jpg?v=1716110328",
+      name: "[Product Name 10]",
       photos: 1,
-      date: '23/04/24',
-      time: '3.00 PM',
-      aspect: 'rectangle'
-    }
+      date: "23/04/24",
+      time: "3.00 PM",
+      aspect: "rectangle",
+    },
   ];
 
-  const [isOpen, setIsOpen] = React.useState(false);
+  const cards = historyData
+  // const cards = demoCards;
 
-  const images = cards.map(card => {
+  const images = cards.map((card) => {
     return {
       src: card.src,
-      alt: card.name
+      alt: card.name,
     };
   });
 
   return (
     <div className="flex flex-col bg-white">
-      <HistoryModal images={images} isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      <HistoryModal
+        images={images}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
       <div className=" flex gap-2 items-center mx-7 mt-10 text-sm font-semibold leading-5 text-slate-700 max-md:flex-wrap max-md:mt-10 max-md:mr-2.5">
-          <div className="flex-1 shrink-0 self-stretch my-auto h-px bg-gray-200" />
-          <div className="justify-center self-stretch px-3.5 py-2.5 bg-white rounded-lg border border-gray-300 border-solid shadow-sm">Created Photos</div>
-          <div className="flex-1 shrink-0 self-stretch my-auto h-px bg-gray-200" />
+        <div className="flex-1 shrink-0 self-stretch my-auto h-px bg-gray-200" />
+        <div className="justify-center self-stretch px-3.5 py-2.5 bg-white rounded-lg border border-gray-300 border-solid shadow-sm">
+          Created Photos
         </div>
+        <div className="flex-1 shrink-0 self-stretch my-auto h-px bg-gray-200" />
+      </div>
       <div className="w-full max-md:max-w-full">
-          <main className="grid sm:grid-cols-2 md:grid-cols-5 items-center gap-2.5 mx-5 pt-8 font-medium text-white flex-wrap max-md:mr-2.5 max-md:max-w-full">
-            {cards.map((card, index) => (
-              <Card key={index} {...card} aspect="square" onClick={() => setIsOpen(true)} />
-            ))}
-          </main>
-        </div>
+        <main className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 items-center gap-2.5 mx-5 pt-8 font-medium text-white flex-wrap max-md:mr-2.5 max-md:max-w-full">
+          {cards.map((card, index) => (
+            <Card
+              key={index}
+              {...card}
+              aspect="square"
+              onClick={() => setIsOpen(true)}
+            />
+          ))}
+        </main>
+      </div>
     </div>
   );
 };
--- app/components/HistoryQueue.tsx
+++ app/components/HistoryQueue.tsx
@@ -1,71 +1,151 @@
-import React from 'react';
+import gql from "graphql-tag";
+import React, { FC } from "react";
+import client from "~/services/ApolloClient";
 
-type IconItemProps = {
+type ImageWithAltProps = {
   src: string;
   alt: string;
+  className?: string;
 };
 
-type ImageCardProps = {
+const ImageWithAlt: FC<ImageWithAltProps> = ({ src, alt, className }) => (
+  <img
+    loading="lazy"
+    src={src}
+    alt={alt}
+    className={className}
+    style={{ height: "100%" }}
+  />
+);
+
+type CardProps = {
   src: string;
-  alt: string;
-  label: string;
-  description: string;
+  name: string;
+  photos: number;
   date: string;
+  time: string;
+  aspect: string;
   onClick?: () => void;
 };
 
-const ImageCard: React.FC<ImageCardProps> = ({ src, alt, label, description, date, onClick }) => (
-  <div /* style={{ width: '238px', height: '238px' }} */ className={`flex-shrink-0 flex overflow-hidden relative flex-row justify-center rounded-lg p-2.5  aspect-square`} onClick={onClick}>
-    <img style={{ height: '100%' }} loading="lazy" src={src} alt={alt} className="object-cover absolute inset-0 size-full" />
-    <div className="w-full flex relative flex-col items-start  py-5 px-5 rounded-lg border border-white border-solid shadow-lg backdrop-blur-[6px] max-md:px-5">
-      <div className="text-xl sm:text-lg font-semibold">{label}</div>
-      <div className="text-base mt-[75%] sm:text-sm sm:mt-[50%] md:text-lg md:mt-[50%]">{description}</div>
-      <div className="text-base sm:text-sm md:text-lg mt-2.5">{date}</div>
+const Card: FC<CardProps> = ({
+  src,
+  name,
+  photos,
+  date,
+  time,
+  aspect,
+  onClick,
+}) => (
+  <div
+    // style={{ width: '238px', height: '238px' }}
+    onClick={onClick}
+    className={`cursor-pointer flex-shrink-0 flex overflow-hidden relative flex-col justify-center rounded-lg p-2.5 ${aspect === "square" ? "aspect-square" : "aspect-[1.34]"}`}
+  >
+    <ImageWithAlt
+      src={src}
+      alt={name}
+      className=" object-cover absolute inset-0 size-full"
+    />
+    <div className="flex flex-col h-full items-start py-5 px-5 rounded-lg border border-white border-solid shadow-lg backdrop-blur-[6px] max-md:pr-5">
+      <div className="text-xl sm:text-md md:text-xl lg:text-base xl:text-2xl font-semibold">
+        {name}
+      </div>
+      <div className="text-base sm:text-base md:text-lg lg:text-sm xl:text-xl mt-auto">
+        {photos} Photos
+      </div>
+      <div className="text-base sm:text-base md:text-lg lg:text-sm xl:text-xl">
+        {date} | {time}
+      </div>
     </div>
   </div>
 );
 
 const HistoryQueue: React.FC = () => {
-  const openModal = () => {
-    const modal = document.getElementById('history-modal');
-    if (modal) {
-      modal.style.display = 'block';
-    }
+  const [historyData, setHistoryData] = React.useState([]);
+
+  const fetchHistoryData = async () => {
+    await client
+      .query({
+        query: gql`
+          query MyQuery {
+            product_photo_history {
+              generated_image
+              created_at
+              uuid
+              store_product {
+                images
+                product_id
+                title
+              }
+            }
+          }
+        `,
+        fetchPolicy: "network-only",
+      })
+      .then((result) => {
+        console.log("apollo client result: :::", result);
+        var product_photo_history = result.data.product_photo_history;
+        // Map over store_products to create a new array with the added 'image' property
+        function formatTimeWithAMPM(dateTimeString:string) {  
+          // 解析日期时间字符串  
+          const dateTime = new Date(dateTimeString);  
+        
+          // 提取小时和分钟  
+          const hours = dateTime.getHours();  
+          const minutes = dateTime.getMinutes().toString().padStart(2, '0');  
+        
+          // 判断AM/PM并添加标记  
+          const ampm = hours >= 12 ? 'PM' : 'AM';  
+          const formattedHours = hours % 12 || 12; // 将24小时制转换为12小时制，并处理午夜的情况  
+        
+          // 格式化时间并返回  
+          return `${formattedHours}:${minutes} ${ampm}`;  
+      }  
+        const updatedStoreProducts = product_photo_history.map(
+          (pph: any, index: number) => {
+            //   // Assuming sp.images is already a JSON string that needs to be parsed
+            return {
+              src: 'https://cdn.builder.io/api/v1/image/assets/TEMP/41393cd4cd5332521a6a76c7311cd6bf12859d641f3850f2f131af1897cc1406?apiKey=f33f54c3e98c47d08e772cdbeee9d64d&',
+              name: pph.store_product.title,
+              photos: 'Creating',
+              date: pph.created_at.split("T")[0],
+              time: formatTimeWithAMPM(pph.created_at)
+            };
+          }
+        );
+
+        setHistoryData(updatedStoreProducts);
+        console.log(
+          "apollo client updatedStoreProductQueues: :::",
+          updatedStoreProducts
+        );
+      });
   };
+
+  
+  React.useEffect(() => {
+    // Call the fetchProducts function when the component mounts
+    console.log("apollo useEffect :::");
+    fetchHistoryData();
+  }, []);
+
+  const cards = historyData.slice(0, 4);
 
   return (
     <div className=" bg-white">
       <div className="w-full max-md:max-w-full">
-        <main className="grid sm:grid-cols-2 md:grid-cols-5 flex-wrap items-center gap-2.5 mx-5 pt-8 font-medium text-white max-md:flex-wrap max-md:mr-2.5 max-md:max-w-full">
-          <ImageCard
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/41393cd4cd5332521a6a76c7311cd6bf12859d641f3850f2f131af1897cc1406?apiKey=f33f54c3e98c47d08e772cdbeee9d64d&"
-            alt="Product 1 image"
-            label="[Product Name 2]"
-            description="Creating Photos"
-            date="23/04/24 | 3.00 PM"
-          />
-          <ImageCard
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/b3654aa7365c184885b94e7b1ef6372d1e9fdac9cd754fda6a5989ec23025be7?apiKey=f33f54c3e98c47d08e772cdbeee9d64d&"
-            alt="Product 2 image"
-            label="[Product Name 2]"
-            description="Creating Photos"
-            date="23/04/24 | 3.00 PM"
-          />
-          <ImageCard
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/b3654aa7365c184885b94e7b1ef6372d1e9fdac9cd754fda6a5989ec23025be7?apiKey=f33f54c3e98c47d08e772cdbeee9d64d&"
-            alt="Product 3 image"
-            label="[Product Name 2]"
-            description="Creating Photos"
-            date="23/04/24 | 3.00 PM"
-          />
-          <ImageCard
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/b3654aa7365c184885b94e7b1ef6372d1e9fdac9cd754fda6a5989ec23025be7?apiKey=f33f54c3e98c47d08e772cdbeee9d64d&"
-            alt="Product 4 image"
-            label="[Product Name 2]"
-            description="Creating Photos"
-            date="23/04/24 | 3.00 PM"
-          />
-          <div
+        <main className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 flex-wrap items-center gap-2.5 mx-5 pt-8 font-medium text-white max-md:flex-wrap max-md:mr-2.5 max-md:max-w-full">
+          {cards.map((card, index) => (
+            <Card
+              key={index}
+              {...card}
+              aspect="square"
+            />
+          ))}
+          
+          {historyData.length > 4 && (
+            <div
             // style={{ width: '238px', height: '238px' }}
             className="flex flex-shrink-0 overflow-hidden relative flex-col justify-center p-2.5 text-base font-black leading-6 text-center aspect-square"
           >
@@ -75,13 +155,14 @@ const HistoryQueue: React.FC = () => {
               alt="Background"
               className="object-cover absolute inset-0 size-full"
             />
-            <div className="flex justify-center align-center text-xl sm:text-lg items-center px-16 pb-2.5 rounded-lg border border-white border-solid shadow-lg aspect-square backdrop-blur-[6px] max-md:px-5">
-              + 2 more <br /> in queue
+            <div className="text-xl sm:text-xl md:text-xl lg:text-base xl:text-xl font-semibold flex justify-center align-center items-center pb-2.5 rounded-lg border border-white border-solid shadow-lg aspect-square backdrop-blur-[6px] max-md:px-5">
+              + {historyData.length - 4} more <br /> in queue
             </div>
           </div>
+          )}）
+          
         </main>
-       
-        {/* <img style={{ marginLeft: "auto", marginRight: "auto", width: "50%" }} loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/ecb2b0acc472a4ad736d879ed5de52f284a0bebb466bee5e147ef9321db10133?apiKey=f33f54c3e98c47d08e772cdbeee9d64d&" alt="Illustration" className="self-center mt-0 max-w-full aspect-[1.06] w-[512px]" /> */}
+
       </div>
     </div>
   );
