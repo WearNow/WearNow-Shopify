134975031d7362cab96aec133e7571948a28bbcd
Saurab Bhatia <S.Bhatiaodesk@gmail.com>
2024-06-03 17:21:50 +0530

    virtualtryon

--- app/components/MultipleSearchSelection.tsx
+++ app/components/MultipleSearchSelection.tsx
@@ -26,8 +26,8 @@ const MultipleSearchSelection: React.FC<Props> = ({ options }) => {
     );
 
     return (
-        <div className="flex flex-col w-full">
-            <div className="relative border border-gray-300 rounded">
+        <div className="flex flex-col w-full ">
+            <div className="relative border border-gray-300 rounded"style={{border:"1px solid #858585",borderRadius:"10px"}}>
             <svg className="search_icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
             <path d="M17.5 17.5L14.5834 14.5833M16.6667 9.58333C16.6667 13.4954 13.4954 16.6667 9.58333 16.6667C5.67132 16.6667 2.5 13.4954 2.5 9.58333C2.5 5.67132 5.67132 2.5 9.58333 2.5C13.4954 2.5 16.6667 5.67132 16.6667 9.58333Z" stroke="#667085" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
@@ -48,6 +48,7 @@ const MultipleSearchSelection: React.FC<Props> = ({ options }) => {
                     ))}
                 </div>
                 <input
+                   style={{borderRadius:"10px"}}
                     type="text"
                     value={searchTerm}
                     onChange={(e) => {
@@ -55,7 +56,7 @@ const MultipleSearchSelection: React.FC<Props> = ({ options }) => {
                         setShowOptions(!!e.target.value); // Show options when there's a search term
                     }}
                     onFocus={() => setShowOptions(!!searchTerm)} // Show options when input is focused and there's a search term
-                    className="pl-[40px] pt-[10px] pb-[10px] w-full focus:outline-none"
+                    className="pl-[40px] pt-[5px] pb-[5px] w-full focus:outline-none"
                 />
                 {showOptions && (
                     <ul className="absolute z-10 bg-white w-full border border-gray-300 rounded mt-1 overflow-y-auto max-h-48">
@@ -76,3 +77,4 @@ const MultipleSearchSelection: React.FC<Props> = ({ options }) => {
 };
 
 export default MultipleSearchSelection;
+ 
