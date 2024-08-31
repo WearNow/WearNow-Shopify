d097209600747cbfd7a23e941a6696d5ee232678
Amen Abe <47492590+amenabe22@users.noreply.github.com>
2024-08-31 11:38:49 +0300

    Update route.tsx

--- app/routes/api.fetchDbData/route.tsx
+++ app/routes/api.fetchDbData/route.tsx
@@ -18,7 +18,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   let auth_session = {};
   switch (queryfor) {
     case "acknowledgements":
-      let acknowledgement=[];
+      let acknowledgement = [];
       await client
         .query({
           query: gql`
@@ -39,18 +39,18 @@ export const action = async ({ request }: ActionFunctionArgs) => {
           },
         })
         .then((result) => {
-          console.log(result,"result for acknowledgement");
-          acknowledgement=result.data.vto_image_generation_request_by_pk;
+          console.log(result, "result for acknowledgement");
+          acknowledgement = result.data.vto_image_generation_request_by_pk;
         });
-        return cors(
-          request,
-          json({
-            success: true,
-            message: "Selected VTO products fetched successfully",
-            response: acknowledgement,
-            request_id: body.get('REQUEST_ID'),
-          })
-        );
+      return cors(
+        request,
+        json({
+          success: true,
+          message: "Selected VTO products fetched successfully",
+          response: acknowledgement,
+          request_id: body.get('REQUEST_ID'),
+        })
+      );
       break;
     case "checkVirtualTryOn":
       await client
@@ -109,7 +109,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
           storeid: auth_session.store_id,
         },
       });
-      console.log(result,"enabled Products");
+      console.log(result, "enabled Products");
       var store_products = result.data.store_products;
 
 
@@ -122,7 +122,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
         })
       );
       break;
-      case "createVirtualTryOn":
+    case "createVirtualTryOn":
       await client
         .query({
           query: gql`
@@ -176,17 +176,17 @@ export const action = async ({ request }: ActionFunctionArgs) => {
         mutation: mutation,
         fetchPolicy: "network-only",
         variables: {
-          gender:body.get('gender'),
-          size:body.get('size'),
-          skin_composition:body.get('skin_composition'),
-          input_image:body.get('input_image'),
+          gender: body.get('gender'),
+          size: body.get('size'),
+          skin_composition: body.get('skin_composition'),
+          input_image: body.get('input_image'),
           storeid: auth_session.store_id,
-          store_product_id:body.get('store_product_id'),
+          store_product_id: body.get('store_product_id'),
           //store_product_id:"e8a6ada9-1dbb-4e12-bc0b-13ff8c004257",
-          model_id:body.get('model_id'),
+          model_id: body.get('model_id'),
         },
       });
-      console.log(muation_result,"muation_result Products");
+      console.log(muation_result, "muation_result Products");
       return cors(
         request,
         json({
@@ -197,7 +197,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
       );
       break;
     case "extpopup":
-      
+
       await client
         .query({
           query: gql`
@@ -220,35 +220,109 @@ export const action = async ({ request }: ActionFunctionArgs) => {
           auth_session = result?.data.session[0] ?? result?.data.session;
           console.log(result.data.session, "apollo client");
         });
-        let extresult;
-        if(!auth_session?.store?.extension_enabled) {
-          const MUTATION12 = gql`mutation MyMutation12($storeid:uuid) {
+      if (!auth_session?.store?.extension_enabled) {
+        const MUTATION12 = gql`mutation MyMutation12($storeid:uuid) {
             update_stores(where: {uuid: {_eq: $storeid}}, _set: {extension_enabled: true}) {
               affected_rows
             }
           }`;
-           extresult = await client.mutate({
-            mutation: MUTATION12,
-            fetchPolicy: "network-only",
-            variables: {
-              storeid: auth_session?.store_id,
-            },
-          });
+        const result = await client.mutate({
+          mutation: MUTATION12,
+          fetchPolicy: "network-only",
+          variables: {
+            storeid: auth_session?.store_id,
+          },
+        });
+      }
+      break;
+    case "getModelsHistorySet":
+
+      await client
+        .query({
+          query: gql`
+            query MyQuery2($shop: String!) {
+              session(limit: 10, where: {shop_id: {_eq: $shop}}) {
+                store_id
+                store {
+                  uuid
+                  extension_enabled
+                }
+              }
+            }
+          `,
+          fetchPolicy: "network-only",
+          variables: {
+            shop: shop,
+          },
+        })
+        .then((result) => {
+          auth_session = result?.data.session[0] ?? result?.data.session;
+          console.log(auth_session, "<<||||apollo client");
+        });
+      const queryMset = gql`query product_photo_history($store_id: uuid!) {
+        product_photo_history(
+          limit: 100, 
+          order_by: {created_at: desc}, 
+          where: {request: {store_id: {_eq: $store_id}}
+          }) {
+          uuid
+          created_at
+          public
+          approved
+          generated_image
+          gender
+          request {
+            model_id
+            model{
+              name
+              mask_image
+              lower_mask
+              upper_mask
+              cover_image
+            }
+            background_id
+            generated_count
+            public
+            pose_id
+            store_id
+            store_product_id
+          }
+          store_product {
+            title
+            store_id
+          }
         }
-        return cors(
-          request,
-          json({
-            success: true,
-            message: "Selected VTO products fetched successfully",
-            response: extresult,
-          })
-        );
+      }
+      `
+      console.log("auth_session.store_id: ", auth_session)
+      // console.log(getModelsHistorySet, "querym");
+      const resultmSet = await client.query({
+        query: queryMset,
+        fetchPolicy: "network-only",
+        variables: {
+          store_id: auth_session.store_id,
+        },
+      });
+
+      console.log(resultmSet);
+
+      var store_products = resultmSet.data.product_photo_history;
+
+
+      return cors(
+        request,
+        json({
+          success: true,
+          message: "Generated photos fetched successfully",
+          response: store_products,
+        })
+      );
       break;
     case "checkModels":
       let variant_id = body.get('variantID');
-      variant_id=variant_id?.replace("gid://shopify/ProductVariant/","");
-      variant_id="gid://shopify/ProductVariant/"+variant_id;
-      console.log(variant_id,"variantID");
+      variant_id = variant_id?.replace("gid://shopify/ProductVariant/", "");
+      variant_id = "gid://shopify/ProductVariant/" + variant_id;
+      console.log(variant_id, "variantID");
       const querym = gql`query MyQuery3($variant_id: String!){
           product_photo_history(where:{
             store_product:{
@@ -273,7 +347,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
             }
           }
         }`;
-      console.log(querym,"querym");
+      console.log(querym, "querym");
       const resultm = await client.query({
         query: querym,
         fetchPolicy: "network-only",
@@ -283,7 +357,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
       });
 
       console.log(resultm);
- 
+
       var store_products = resultm.data.product_photo_history;
 
 
